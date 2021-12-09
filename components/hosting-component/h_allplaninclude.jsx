c51cff3f0e0900a47ae4d2f3930f8e18adc28344
ochibaihaqqi <ochibaihaqqi2000@gmail.com>
2021-12-09 07:44:37 +0700

    Create h_allplaninclude.jsx

---
+++ components/hosting-component/h_allplaninclude.jsx
@@ -0,0 +1,81 @@
+export default function H_allplaninclude() {
+	return (
+		<>
+		    <section className="h_allplaninclude">
+      <div className="container">
+        
+        <div className="title">
+          <span id="h1">All Plans</span>
+          <span id="space"> </span>
+          <span id="h2">Include</span>
+        </div>
+
+        <div className="plan_include">
+          
+          <div className="row">
+            
+            <div className="frame">
+                <div className="title">
+                  <span id="h1">Easy Website</span>
+                  <span id="space"> </span>
+                  <span id="h2">Creation</span>
+                </div>
+                <p>Experience provides an easy-to-use WordPress website builder to make creating a professional-looking website fast and easy.</p>
+            </div>
+
+            <div className="frame">
+                <div className="title">
+                  <span id="h1">Resource</span>
+                  <span id="space"> </span>
+                  <span id="h2">Protection</span>
+                </div>
+                <p>Even if there are other websites on the shared server, your site’s performance stays protected and unaffected.</p>
+            </div>
+
+            <div className="frame">
+                <div className="title">
+                  <span id="h1">Domain</span>
+                  <span id="space"> </span>
+                  <span id="h2">Manager</span>
+                </div>
+                <p>Our domain manager makes it easy to update, transfer, purchase and manage your domains in one place.</p>
+            </div>
+          
+          </div>
+          <div className="row">
+            
+            <div className="frame">
+              <div className="title">
+                  <span id="h1">SSL</span>
+                  <span id="space"> </span>
+                  <span id="h2">Certificates</span>
+                </div>
+                <p>Keep personal information, e-commerce transactions and other sensitive data safeguarded with encrypted connections.</p>
+            </div>
+
+            <div className="frame">
+                <div className="title">
+                  <span id="h1">Google</span>
+                  <span id="space"> </span>
+                  <span id="h2">Ads</span>
+                </div>
+                <p>Give your campaign an extra kick with a Google Ads match credit up to $150 on your first campaign (valid for new Google Ads customers located in US).</p>
+            </div>
+
+            <div className="frame">
+                <div className="title">
+                  <span id="h1">Google</span>
+                  <span id="space"> </span>
+                  <span id="h2">My Business</span>
+                </div>
+                <p>List your local business online, including its location and hours of operation, to quickly connect with customers in your area.</p>
+            </div>
+          
+          </div>
+        
+        </div>
+      </div>
+    </section>
+		</>
+	)
+}
