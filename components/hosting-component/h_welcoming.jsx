da14dbef072b22bf0f64209547a0cdab4026dad7
ochibaihaqqi <ochibaihaqqi2000@gmail.com>
2021-12-09 07:44:42 +0700

    Create h_welcoming.jsx

---
+++ components/hosting-component/h_welcoming.jsx
@@ -0,0 +1,23 @@
+export default function H_welcoming() {
+	return(
+		<>
+		<section className='h_welcoming'>
+          <div className="container">
+
+            <div className="content">
+                <div className='title'>
+                  <h1>Hosting Service And More</h1>
+                </div>
+                <div className='subtitle mt-4'>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ultricies etiam proin vel malesuada aliquam est. Ut accumsan, accumsan augue ac lectus sed faucibus. Tincidunt varius.</p>
+                </div>
+            </div>
+
+            <div className="illustrator">
+              <img src="hosting/welcoming/welcome-hosting-illustration.svg" alt="" />
+            </div>
+          </div>
+    	</section>
+		</>
+	)
+}
