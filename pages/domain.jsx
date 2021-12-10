3d1013a58de43142101bbf210bac4a0b4244e5be
ochibaihaqqi <ochibaihaqqi2000@gmail.com>
2021-12-10 21:13:30 +0700

    Create domain.jsx

---
+++ pages/domain.jsx
@@ -0,0 +1,18 @@
+import Head from "next/head"
+import Navbar from "../components/Navbar"
+import Footer from "../components/footer"
+
+export default function Hosting () {
+	return (
+		<>
+		<Head>
+        	<title>Experience | Domain</title>
+    	</Head>
+
+		<Navbar/>
+		
+
+		<Footer/>
+		</>
+	)
+}
