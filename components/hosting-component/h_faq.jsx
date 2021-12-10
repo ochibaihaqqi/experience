d48bc216546284d3d882edc32effe4b1861f5201
ochibaihaqqi <ochibaihaqqi2000@gmail.com>
2021-12-10 21:13:26 +0700

    Create h_faq.jsx

---
+++ components/hosting-component/h_faq.jsx
@@ -0,0 +1,48 @@
+export default function H_faq() {
+	return (
+		<>
+		<section className="h_faq">
+			<div className="container">
+
+				<div className="title">
+					<h1>Frequently Asked</h1>
+					<h2>Questions</h2>
+				</div>
+
+				<div className="frame">
+					<div className="break_line"></div>
+					<div className="title">
+						<h1>What is Shared Hosting?</h1>
+					</div>
+					<p>Shared Hosting is one of the most popular hosting packages because it provides an affordable way to get your website online. Shared Hosting works by allowing multiple users to share space on a single server maintained by the hosting provider (Bluehost). A Shared Hosting package can include multiple offers or combinations of features as offered by their hosting provider. Bluehost Shared Hosting is perfect for blogging, hobbies and small business websites.</p>
+				</div>
+
+				<div className="frame">
+					<div className="break_line"></div>
+					<div className="title">
+						<h1>What are Shared Hosting Benefits?</h1>
+					</div>
+					<p>When you sign up for a Shared Hosting package, your biggest benefit is purchasing hosting at an affordable price. In addition to various features, our shared hosting platform is managed which means users don’t have to worry about server management and platform patches. When you purchase a Bluehost Shared Hosting package it also includes an SSL certificate, domain name and more!</p>
+				</div>
+
+				<div className="frame">
+					<div className="break_line"></div>
+					<div className="title">
+						<h1>How do I get started with Shared Hosting?</h1>
+					</div>
+					<p>Bluehost strives to make it easy for our customers to get their website online with the purchase of our hosting packages. When you visit the Bluehost website you will select your Shared Web Hosting plan. These plans vary in prices and features. Once you select your hosting package we will automatically install the latest version of WordPress for you. Once you’re logged in, it’s time to get your website online!</p>
+				</div>
+
+				<div className="frame">
+					<div className="break_line"></div>
+					<div className="title">
+						<h1>How do I migrate to Bluehost?</h1>
+					</div>
+					<p>If you already have a website and are considering changing your web host, then you will need some assistance with migration. Bluehost provides a migration service that can assist you in getting all your files transferred securely and correctly. Migrating your website can be time consuming and extremely complex so we want to provide you with the best support possible from our team of experts.</p>
+				</div>
+
+			</div>
+    	</section>
+		</>
+	)
+}
