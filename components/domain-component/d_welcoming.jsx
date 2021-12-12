0454e7b4ebf23e54a1a56c3e74f6990089e8a9dc
ochibaihaqqi <ochibaihaqqi2000@gmail.com>
2021-12-12 18:25:21 +0700

    Create d_welcoming.jsx

---
+++ components/domain-component/d_welcoming.jsx
@@ -0,0 +1,23 @@
+export default function D_welcoming() {
+	return(
+		<>
+		<section className='d_welcoming'>
+          <div className="container">
+
+            <div className="content">
+                <div className='title'>
+                  <h1>Domain Service <br /> And More</h1>
+                </div>
+                <div className='subtitle mt-4'>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ultricies etiam proin vel malesuada aliquam est. Ut accumsan, accumsan augue ac lectus sed faucibus. Tincidunt varius.</p>
+                </div>
+            </div>
+
+            <div className="illustrator">
+              <img src="domain/welcoming/welcome-domain-illustration.svg" alt="" />
+            </div>
+          </div>
+    	</section>
+		</>
+	)
+}
